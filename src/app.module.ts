6640ba56665a8199091648772bc0f409c4cd9889
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2025-06-05 10:17:17 -0300

    build: add application to the same docker-compose network

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgres-template',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
